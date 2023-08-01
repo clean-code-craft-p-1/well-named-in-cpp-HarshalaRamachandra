18c43536361518a4c81e6193b5d5ff79596b0613
Harshala Ramachandra <Harshala.R@philips.com>
2023-08-01 23:37:35 +0530

    Assignment2Trial

--- main.cpp
+++ main.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <assert.h>
-#include "TelCoColorCoder.h" // Include the header file
+#include "ColorCodes.h"
+#include "ColorPair.h"
+#include "ColorConverter.h"
 
-void testNumberToPair(int pairNumber,
-	TelCoColorCoder::MajorColor expectedMajor,
-	TelCoColorCoder::MinorColor expectedMinor)
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor)
 {
-	// ... rest of the function definitions ...
-	TelCoColorCoder::ColorPair colorPair =
-		TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-	std::cout << "Got pair " << colorPair.ToString() << std::endl;
-	assert(colorPair.getMajor() == expectedMajor);
-	assert(colorPair.getMinor() == expectedMinor);
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
 }
 
-void testPairToNumber(TelCoColorCoder::MajorColor major,
-	TelCoColorCoder::MinorColor minor,
-	int expectedPairNumber)
+void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber)
 {
-	// ... rest of the function definitions ...
-	int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-	std::cout << "Got pair number " << pairNumber << std::endl;
-	assert(pairNumber == expectedPairNumber);
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
 }
 
 int main() {
-	testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-	testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-	testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-	testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    
+    TelCoColorCoder::printColorCodingRefManual();  //Print Color-Name to Correspoding Pair Number reference manual
 
-	return 0;
+    return 0;
 }
