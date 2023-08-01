bee45a63a444ebcff92553ccb8c24d1ebc6f2274
Harshala Ramachandra <Harshala.R@philips.com>
2023-08-01 12:04:18 +0530

    Assignment-2 PartA

---
+++ TelCoColorCoder.h
@@ -0,0 +1,24 @@
+#ifndef TELCO_COLOR_CODER_H
+#define TELCO_COLOR_CODER_H
+
+#include <string>
+
+namespace TelCoColorCoder
+{
+	enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+	enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+	// ... rest of the declarations from the namespace ...
+	const char* MajorColorNames[] = {
+		"White", "Red", "Black", "Yellow", "Violet"
+	};
+	int numberOfMajorColors =
+		sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+	const char* MinorColorNames[] = {
+		"Blue", "Orange", "Green", "Brown", "Slate"
+	};
+	class ColorPair {};
+}
+
+#endif // TELCO_COLOR_CODER_H
+#pragma once
